2338c0410e3255fa571874b48783a654949fa2b0
Vladislav Yundin <4yundin@gmail.com>
2022-04-12 14:19:58 +0400

    feature: create design components and use in search

---
+++ sources/base/designsystem/src/main/java/com/yundin/designsystem/theme/components/Components.kt
@@ -0,0 +1,98 @@
+package com.yundin.designsystem.theme.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchField(value: String, onValueChange: (String) -> Unit, label: String) {
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(text = label)
+        }
+    )
+}
+
+data class UIRepository(
+    val name: String,
+    val description: String?
+)
+
+@Composable
+fun RepositoryCard(repository: UIRepository, onClick: () -> Unit) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 4.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable(onClick = onClick)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = repository.name,
+                style = MaterialTheme.typography.h6
+            )
+            Text(
+                text = repository.description ?: "No description",
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Composable
+fun AnimatedVisibilityProgressBarItem(visible: Boolean) {
+    AnimatedVisibility(visible = visible) {
+        ProgressBarItem()
+    }
+}
+
+@Composable
+fun ProgressBarItem() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun LoadingFailedItem(onRetryClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Loading failed",
+            style = MaterialTheme.typography.body1
+        )
+        Button(onClick = onRetryClick) {
+            Text(
+                text = "Retry",
+            )
+        }
+    }
+}
--- sources/features/search/src/main/java/com/yundin/search/SearchUI.kt
+++ sources/features/search/src/main/java/com/yundin/search/SearchUI.kt
@@ -1,21 +1,26 @@
 package com.yundin.search
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import com.yundin.datasource.Repository
+import com.yundin.designsystem.theme.components.*
 import com.yundin.navigation.SearchScreens
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -36,21 +41,62 @@ fun SearchUI(viewModel: SearchViewModel) {
     val lazyPagingItems = viewModel.searchResult.collectAsLazyPagingItems()
     val request: String by viewModel.searchRequest.observeAsNonNullState()
     Column {
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = request,
-            onValueChange = viewModel::onInputChange
-        )
-        LazyColumn {
-            items(lazyPagingItems) {
-                Column {
-                    Text("Name is ${it?.fullName}")
-                    Text("Desc is ${it?.description}")
-                }
-            }
-        }
+        SearchField(value = request, onValueChange = viewModel::onInputChange, label = "GitHub repository search")
+        Divider()
+        SearchResults(lazyPagingItems = lazyPagingItems)
     }
 }
 
 @Composable
-fun <T> LiveData<T>.observeAsNonNullState(): State<T> = observeAsState(value!!)
+private fun SearchResults(
+    lazyPagingItems: LazyPagingItems<Repository>
+) {
+    val lazyListState = rememberLazyListState()
+    LaunchedEffect(lazyPagingItems.loadState.refresh) {
+        // new query, scroll to top
+        lazyListState.scrollToItem(0, 0)
+    }
+    LazyColumn(state = lazyListState) {
+        itemProgressBarForState(lazyPagingItems.loadState.refresh)
+        itemsRepositories(lazyPagingItems)
+        itemProgressBarForState(lazyPagingItems.loadState.append)
+        itemLoadingError(lazyPagingItems)
+    }
+}
+
+private fun LazyListScope.itemProgressBarForState(loadState: LoadState) {
+    item {
+        AnimatedVisibilityProgressBarItem(
+            visible = loadState is LoadState.Loading
+        )
+    }
+}
+
+private fun LazyListScope.itemLoadingError(lazyPagingItems: LazyPagingItems<Repository>) {
+    item {
+        val refreshFailed = lazyPagingItems.loadState.refresh is LoadState.Error
+        val appendFailed = lazyPagingItems.loadState.append is LoadState.Error
+        AnimatedVisibility(
+            visible = refreshFailed || appendFailed
+        ) {
+            LoadingFailedItem { lazyPagingItems.retry() }
+        }
+    }
+}
+
+private fun LazyListScope.itemsRepositories(
+    lazyPagingItems: LazyPagingItems<Repository>
+) {
+    items(lazyPagingItems) { repository ->
+        repository?.let {
+            val uiRepository = UIRepository(it.fullName, it.description)
+            RepositoryCard(repository = uiRepository) {
+                // click
+            }
+        }
+    }
+
+}
+
+@Composable
+private fun <T> LiveData<T>.observeAsNonNullState(): State<T> = observeAsState(value!!)
